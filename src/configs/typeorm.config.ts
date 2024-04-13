7f4466f12cfc4cc95d1c504f6553625a88df372c
SeungWon <iwon@kookmin.ac.kr>
2024-04-12 21:35:21 -0700

    testing

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,19 +4,23 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export const getTypeOrmConfig = (
   configService: ConfigService,
-): TypeOrmModuleOptions => ({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'capstone',
-  entities: [__dirname + '/../**/*.entity.{ts, js}'],
-  synchronize: true,
-  autoLoadEntities: true,
-  namingStrategy: new SnakeNamingStrategy(),
-  logging: true,
-  extra: {
-    timezone: 'UTC',
-  },
-});
+): TypeOrmModuleOptions => {
+  console.log(process.env.DB_HOST);
+  console.log(configService.get('DB_HOST'));
+  return {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1234',
+    database: 'capstone',
+    entities: [__dirname + '/../**/*.entity.{ts, js}'],
+    synchronize: true,
+    autoLoadEntities: true,
+    namingStrategy: new SnakeNamingStrategy(),
+    logging: true,
+    extra: {
+      timezone: 'UTC',
+    },
+  };
+};
