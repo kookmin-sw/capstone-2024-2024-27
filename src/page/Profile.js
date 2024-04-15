8c7fecba6f97653f45d16ed2961ce204ed87b521
DongSeok Son <sds0410@gmail.com>
2024-04-14 22:19:03 -0700

    profile UI

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./Profile.css";
 import axios from "axios";
+import TextField from "@material-ui/core/TextField";
 
 function Profile() {
   const [user, setUser] = useState(null);
+  const [isReadOnly, setIsReadOnly] = useState(true);
+
+  var projects = "1. Project A\n2. project B\n3. Project C";
+  var peopleYouLike = "1. User A\n2. User B\n3. User C";
+  var peopleWhoLikeYou = "1. User A\n2. User B\n3. User C";
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -48,12 +54,60 @@ function Profile() {
       </div>
 
       <div className="profile__section">
-        <h3>Projects you want to do</h3>
-        <textarea className="profile__textarea" readOnly />
+        {/* <h3>Projects you want to do</h3>
+        <textarea
+          className="profile__textarea"
+          readOnly={isReadOnly}
+          value={projects}
+        />
         <h3>People you like</h3>
-        <textarea className="profile__textarea" readOnly />
+        <textarea
+          className="profile__textarea"
+          readOnly={isReadOnly}
+          value={peopleYouLike}
+        />
         <h3>People who like you</h3>
-        <textarea className="profile__textarea" readOnly />
+        <textarea
+          className="profile__textarea"
+          readOnly={isReadOnly}
+          value={peopleWhoLikeYou}
+        /> */}
+        <h3>Projects you want to do</h3>
+        <TextField
+          id="profile__textarea"
+          multiline
+          rows={5}
+          defaultValue={peopleWhoLikeYou}
+          variant="outlined"
+          fullWidth
+          InputProps={{
+            readOnly: isReadOnly,
+          }}
+        />
+        <h3>People you like</h3>
+        <TextField
+          id="profile__textarea"
+          multiline
+          rows={5}
+          defaultValue={peopleWhoLikeYou}
+          variant="outlined"
+          fullWidth
+          InputProps={{
+            readOnly: isReadOnly,
+          }}
+        />
+        <h3>People who like you</h3>
+        <TextField
+          id="profile__textarea"
+          multiline
+          rows={5}
+          defaultValue={peopleWhoLikeYou}
+          variant="outlined"
+          fullWidth
+          InputProps={{
+            readOnly: isReadOnly,
+          }}
+        />
       </div>
     </div>
   );
