2622d644162764d3151902795771afdb757e6779
DongSeok Son <sds0410@gmail.com>
2024-04-24 16:48:54 -0700

    ui reworked

---
+++ src/page/Home.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Home.css";
+
+import TinderCards from "../components/TinderCards";
+import SwipeButtons from "../components/SwipeButtons";
+
+function Home() {
+  const [people, setPeople] = useState([
+    {
+      name: "steve jobs",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg/1280px-Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg",
+    },
+    {
+      name: "mark zuckerberg",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/1280px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg",
+    },
+    {
+      name: "ahri",
+      url: require("../assets/IMG_0870.jpeg"),
+    },
+  ]);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handleButtonClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % people.length);
+  };
+
+  return (
+    <div className="home">
+      <h1>Project Title</h1>
+      <TinderCards people={people[currentIndex]} />
+      <div className="home__textbox">
+        <textarea readOnly value="프로젝트 설명" />
+      </div>
+      <SwipeButtons onButtonClick={handleButtonClick} />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/page/Login2.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#3376cd",
+    "&:hover": {
+      backgroundColor: "#82c2ff",
+    },
+  },
+}));
+
+export default function SignIn({ onLoginSuccess, onSignUpClick }) {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://52.79.82.218:8000/user/login", {
+        email,
+        password,
+      });
+      const { accessToken } = response.data;
+      localStorage.setItem("accessToken", accessToken);
+      onLoginSuccess();
+    } catch (error) {
+      console.error("Login error:", error);
+      alert(`Error code: ${error.code}, Login failed.`);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={handleLogin}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs></Grid>
+            <Grid item>
+              <Link href="#" variant="body2" onClick={onSignUpClick}>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
