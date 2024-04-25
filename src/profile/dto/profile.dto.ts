275820c751aa1edf275a250948ac4fe848320940
SeungWon <iwon@kookmin.ac.kr>
2024-04-25 10:51:47 -0700

    :sparkles: add api docs

--- src/profile/dto/profile.dto.ts
+++ src/profile/dto/profile.dto.ts
@@ -1,28 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ProfileDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'KMUlee',
+  })
   readonly name: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'capstone',
+  })
   readonly title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'This is a capstone project.',
+  })
   readonly description: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'https://github.com/KMUlee',
+  })
   readonly githubLink: string;
 }
 
 export class GetProfileDto extends ProfileDto {
   @Type(() => ProfileDto)
+  @ApiProperty()
   likedByUsers: ProfileDto[] | [];
 
   @Type(() => ProfileDto)
+  @ApiProperty()
   likedProjects: ProfileDto[] | [];
 }
