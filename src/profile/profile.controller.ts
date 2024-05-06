f1de6f90df940953822d395ed7ce7105de6c21ed
SeungWon <iwon@kookmin.ac.kr>
2024-05-06 00:47:13 -0700

    :sparkles: add image upload and download

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -6,7 +6,9 @@ import {
   Param,
   Post,
   Put,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { JwtAuthGuard } from 'src/user/user.guard';
@@ -14,12 +16,41 @@ import { GetUser } from 'src/user/decorators/GetUser.decorator';
 import { Payload } from 'src/user/dto/jwt-payload.dto';
 import { ApiBearerAuth, ApiBody, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { GetProfileDto, ProfileDto } from './dto/profile.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('profile')
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth('access-token')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
+
+  @Get('/image')
+  async getProfileImage(@GetUser() user: Payload): Promise<{ src: string }> {
+    return { src: await this.profileService.getImage(user.userId) };
+  }
+
+  @Get('/:profileId')
+  @ApiParam({
+    name: 'profileId',
+    type: 'number',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Get a profile',
+    type: ProfileDto,
+  })
+  async getProfileById(@Param('profileId') id: number): Promise<ProfileDto> {
+    return await this.profileService.getProfileById(id);
+  }
+
+  @Post('/image')
+  @UseInterceptors(FileInterceptor('image'))
+  async postProfileImage(
+    @UploadedFile() file: Express.Multer.File,
+    @GetUser() user: Payload,
+  ): Promise<AWS.S3.ManagedUpload.SendData> {
+    return this.profileService.imageUpload(file, user.userId);
+  }
 
   @Get('/')
   @ApiResponse({
@@ -63,19 +94,5 @@ export class ProfileController {
     @Body() profile: ProfileDto,
   ): Promise<ProfileDto> {
     return await this.profileService.updateProfile(user.userId, profile);
-  }
-
-  @Get('/:profileId')
-  @ApiParam({
-    name: 'profileId',
-    type: 'number',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Get a profile',
-    type: ProfileDto,
-  })
-  async getProfileById(@Param('profileId') id: number): Promise<ProfileDto> {
-    return await this.profileService.getProfileById(id);
   }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -6,15 +6,24 @@ import { GetProfileDto, ProfileDto } from './dto/profile.dto';
 import { UserService } from 'src/user/user.service';
 import { LikesService } from 'src/likes/likes.service';
 import { User } from 'src/user/entities/user.entity';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class ProfileService {
+  private readonly s3: AWS.S3;
+  private readonly bucketName: string = process.env.AWS_BUCKET_NAME;
   constructor(
     @InjectRepository(Profile)
     private readonly profileRepository: Repository<Profile>,
     private readonly userService: UserService,
     private readonly likesService: LikesService,
-  ) {}
+  ) {
+    this.s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
+  }
 
   async getProfile(userId: number): Promise<GetProfileDto | null> {
     const profile = await this.profileRepository.findOne({
@@ -107,5 +116,82 @@ export class ProfileService {
       throw new HttpException('Profile not found', HttpStatus.NOT_FOUND);
     }
     return profile;
+  }
+
+  async checkAndCreateUserFolder(userId: number): Promise<void> {
+    const folderKey = `${userId}/`;
+
+    try {
+      const params = {
+        Bucket: this.bucketName,
+        Prefix: folderKey,
+        MaxKeys: 1,
+      };
+      const data = await this.s3.listObjectsV2(params).promise();
+      const exists = data.Contents?.length > 0;
+
+      if (!exists) {
+        await this.s3
+          .putObject({
+            Bucket: this.bucketName,
+            Key: folderKey,
+          })
+          .promise();
+      }
+    } catch (error) {
+      console.error(`Error checking or creating user folder: ${error}`);
+      throw new Error('Unable to check or create user folder.');
+    }
+  }
+
+  async fileExists(userId: number): Promise<boolean> {
+    const fileKey = `${userId}/profile.png`;
+
+    try {
+      await this.s3
+        .headObject({
+          Bucket: this.bucketName,
+          Key: fileKey,
+        })
+        .promise();
+      return true;
+    } catch (error) {
+      console.log(error);
+      if (error.code === 'NotFound') {
+        return false;
+      }
+      throw error;
+    }
+  }
+
+  async imageUpload(
+    file: Express.Multer.File,
+    userId: number,
+  ): Promise<AWS.S3.ManagedUpload.SendData> {
+    await this.checkAndCreateUserFolder(userId);
+    const fileKey = `${userId}/profile.png`;
+
+    return this.s3
+      .upload({
+        Bucket: this.bucketName,
+        Key: fileKey,
+        Body: file.buffer,
+        ACL: 'public-read',
+      })
+      .promise();
+  }
+
+  async getImage(userId: number) {
+    const exists = await this.fileExists(userId);
+
+    if (!exists) {
+      throw new HttpException(
+        'User profile not found in S3',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const fileKey = `${userId}/profile.png`;
+
+    return `https://${this.bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`;
   }
 }
