e34782df9dbbc589478febae2281cc84e4d2487b
DongSeok Son <sds0410@gmail.com>
2024-05-15 16:13:41 -0700

    package.json updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ function Header({
   return (
     <div className="header">
       <h1 className="header__text">
-        <img src="codate_color.svg" alt="Logo" className="logo"></img>
+        <img src="codate-color.svg" alt="Logo" className="logo"></img>
         Codate
       </h1>
       {isLoggedin && (
